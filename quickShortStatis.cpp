8b6beb35da4985474a8f818e6c46b12d41684d95
DamaMaulideen <damamaulidin@studets.unnes.ac.id>
2022-04-27 10:40:52 +0700

    first commit

---
+++ quickShortStatis.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int start, int end)
+{
+
+    int pivot = arr[start];
+
+    int count = 0;
+    for (int i = start + 1; i <= end; i++)
+    {
+        if (arr[i] <= pivot)
+            count++;
+    }
+
+    // memposisikan element pivot
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+
+    // sorting bagian kiri dan kanan pivot
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int start, int end)
+{
+
+    // base case
+    if (start >= end)
+        return;
+
+    // mem partisi array
+    int p = partition(arr, start, end);
+
+    // Sorting bagian kiri
+    quickSort(arr, start, p - 1);
+
+    // Sorting bagian kanan
+    quickSort(arr, p + 1, end);
+}
+
+void show(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+
+    int arr[] = {9, 3, 4, 2, 1, 8};
+    int size = 6;
+
+    quickSort(arr, 0, size - 1);
+
+    show(arr, size);
+
+    return 0;
+}
